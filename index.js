d4cd13b8061bae8f534964c775106dc68a048cd9
JfJubaer <skk5727@gmail.com>
2022-12-01 05:08:26 +0600

    6th

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleWire
 
@@ -27,6 +27,12 @@ function run() {
 
         app.get('/categories', async (req, res) => {
             const result = await categoriesCollection.find({}).toArray();
+            res.send(result);
+        })
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await categoriesCollection.findOne(query);
             res.send(result);
         })
         app.get('/products', async (req, res) => {
