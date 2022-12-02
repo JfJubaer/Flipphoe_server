a53addcb1e39f70adb212e6e3d1c9666b672df59
JfJubaer <skk527@gmail.com>
2022-12-02 16:22:16 +0600

    update2

--- index.js
+++ index.js
@@ -1,60 +1,91 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleWire
 
 app.use(cors());
 app.use(express.json());
 
-
-
-const uri = "mongodb+srv://flipphoe:GZHWvETybEReijVB@cluster0.j88am2v.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-
-
-
-
+const uri =
+  "mongodb+srv://flipphoe:GZHWvETybEReijVB@cluster0.j88am2v.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 // databaseSection
 function run() {
-    try {
-        const categoriesCollection = client.db('flipphoeDB').collection('categories');
-        const productsCollection = client.db('flipphoeDB').collection('products');
+  try {
+    const categoriesCollection = client
+      .db("flipphoeDB")
+      .collection("categories");
+    const productsCollection = client.db("flipphoeDB").collection("products");
+    const userCollection = client.db("flipphoeDB").collection("users");
 
-        app.get('/categories', async (req, res) => {
-            const result = await categoriesCollection.find({}).toArray();
-            res.send(result);
-        })
-        app.get('/category/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await categoriesCollection.findOne(query);
-            res.send(result);
-        })
-        app.get('/products', async (req, res) => {
-            const result = await productsCollection.find({}).toArray();
-            res.send(result);
-        })
-    }
-
-    finally {
-
-    }
+    app.get("/categories", async (req, res) => {
+      const result = await categoriesCollection.find({}).toArray();
+      res.send(result);
+    });
+    app.get("/category/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await categoriesCollection.findOne(query);
+      res.send(result);
+    });
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find({}).toArray();
+      res.send(result);
+    });
+    app.post("/login", async (req, res) => {
+      const result = await userCollection.insertOne(req.body);
+      res.send(result);
+    });
+    app.get("/role/:email", async (req, res) => {
+      const user = await userCollection.findOne({ email: req.params.email });
+      if (user.role) {
+        res.send({ role: user.role });
+      } else {
+        res.send({ role: "buyer" });
+      }
+    });
+    app.get("/buyer", async (req, res) => {
+      const result = await userCollection.find({ role: "buyer" }).toArray();
+      res.send(result);
+    });
+    app.get("/seller", async (req, res) => {
+      const result = await userCollection.find({ role: "seller" }).toArray();
+      res.send(result);
+    });
+    app.post("/addproduct", async (req, res) => {
+      console.log(req.body);
+      const result = await res.send({ result: "hello" });
+    });
+    app.post("/addproduct", async (req, res) => {
+      console.log(req.body);
+      const result = await productsCollection.insertOne(req.body);
+      res.send(result);
+    });
+    app.get("/myproducts/:email", async (req, res) => {
+      const result = await productsCollection
+        .find({ email: req.params.email })
+        .toArray();
+      console.log(req.params.email);
+      res.send(result);
+    });
+  } finally {
+  }
 }
-
 
 run();
 
-
-
-app.get('/', (req, res) => {
-    res.send('This is my server of used mobiles')
-})
+app.get("/", (req, res) => {
+  res.send("This is my server of used mobiles");
+});
 
 app.listen(port, () => {
-    console.log('Server is running at ', port)
-})
+  console.log("Server is running at ", port);
+});
